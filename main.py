32eb09997f439c41b4cc8d678819e806a88d7e94
Killerpiez <r.simpkins95@gmail.com>
2023-02-26 12:17:39 -0600

    Changing fib range to 100 to 200

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 fibonacci = [0, 1] # Start with the first two numbers in the sequence
 
-for i in range(2, 100): # Loop from the third number up to the 100th number
+for i in range(2, 200): # Loop from the third number up to the 100th number
     next_number = fibonacci[i-1] + fibonacci[i-2] # Calculate the next number in the sequence
     fibonacci.append(next_number) # Add the next number to the list
 
