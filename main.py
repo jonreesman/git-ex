f609a935de51aabce93d64cb8e40bbd180387c3d
Killerpiez <r.simpkins95@gmail.com>
2023-02-26 12:37:33 -0600

    Added fib.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def fib():
-    fibonacci = [0, 1] # Start with the first two numbers in the sequence
+from fib import fib
 
-    for i in range(2, 200): # Loop from the third number up to the 100th number
-        next_number = fibonacci[i-1] + fibonacci[i-2] # Calculate the next number in the sequence
-        fibonacci.append(next_number) # Add the next number to the list
-
-    print(fibonacci)
-    print("Hello Rickie")
-
-if __name__ == "__main__":
-    fib()
+fib()
